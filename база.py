7579094d3dab3d7aca670f4b81592d754c4de58f
isdj12 <artempop28@gmail.com>
2025-02-19 20:17:12 +0300

    baza

--- база.py
+++ база.py
@@ -1,29 +1,40 @@
-import sqlite3
+from flask import Flask, redirect, url_for, flash, session, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect("ддд.db")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
+db = SQLAlchemy(app)
 
+class Database(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    opis = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    
+    def __init__(self, opis, name):
+        self.opis = opis
+        self.name = name
+        
+with app.app_context():
+    db.create_all()
+    
+@app.route('/', methods=['GET', 'POST'])
+def glavna():
+    if request.method == 'POST':
+        opis = request.form['opis']
+        name = request.form['name']
+        
+        if len(opis) == 0 or len(name) == 0:
+            flash('Opis i ime moraju biti uneti.')
+            return redirect(url_for('glavna'))
+        
+        new_db = Database(opis, name)
+        db.session.add(new_db)
+        db.session.commit()
+        flash('Novi podatak je dodat.')
+        return redirect(url_for('glavna'))
+    
+    db_data = Database.query.all()
+    return render_template('glavna.html', css_file=url_for('glavna.css'))
 
-#создали курсор
-c = db.cursor()
-
-
-#выполняет команду которая в скобках
-#c.execute(""" create table indi (
-   # title text,
-  #  ful_text text,
-   # views integer,
-    #avtor text
-#)            """)
-#обновляет базу данных
-#c.execute("INSERT INTO indi VALUES ('gogl', 'ggg', '2', 'artem')")
-
-
-# выводит все что находится в аблице данных можно и выборочно если назвать нужную ячейку
-c.execute("SELECT rowid, * FROM indi  WHERE rowid = 2")
-items = c.fetchall()
-
-for el in items:
-    print(el [1] + "\n" + el[3])
-db.commit()
-
-db.close()
+if __name__ == '__main__':
+    app.run(debug=True)
